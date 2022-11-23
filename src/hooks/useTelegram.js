74457af329871685f517ec1c659e17a10e41505a
Anton Tokarev <anton999@bk.ru>
2022-11-23 20:41:28 +0500

    134

--- src/hooks/useTelegram.js
+++ src/hooks/useTelegram.js
@@ -1,24 +1,24 @@
 const tg = window.Telegram.WebApp;
 
-export function userTelegram() {
+export function useTelegram() {
 
     const onClose = () => {
         tg.close()
-      }
+    }
 
-      
     const onToggleButton = () => {
-      if(tg.MainButton.isVisible) {
-        tg.MainButton.hide();
-      } else {
-        tg.MainButton.show();
-      }
-      }
+        if(tg.MainButton.isVisible) {
+            tg.MainButton.hide();
+        } else {
+            tg.MainButton.show();
+        }
+    }
 
-return {
-    onClose,
-    onToggleButton,
-    tg, 
-    user: tg.initDataUnsafe?.user,
-}
+    return {
+        onClose,
+        onToggleButton,
+        tg,
+        user: tg.initDataUnsafe?.user,
+        queryId: tg.initDataUnsafe?.query_id,
+    }
 }
